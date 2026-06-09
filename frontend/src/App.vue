7695f9a48bd3796497a12b40cd0bb13916a03fed
Syntyche Djuissi <syntychedjuissi@gmail.com>
2026-06-09 17:14:53 +0200

    frontend

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,3 +1,7 @@
+<script setup>
+import HelloWorld from './components/HelloWorld.vue'
+</script>
+
 <template>
-    <router-view/>
+  <HelloWorld />
 </template>
--- frontend/src/views/loginView.vue
+++
@@ -1,5 +0,0 @@
-<template>
-    <div>
-        <h1>Connexion</h1>
-    </div>
-</template>
